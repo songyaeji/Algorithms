16bfbae746ce5196ba1fd923b86ab715a5f5ceb3
songyaeji <skaehdlf0318@gmail.com>
2025-09-22 15:31:32 +0900

    5597 test 2

--- baekjoon/5597.py
+++ baekjoon/5597.py
@@ -1,8 +1,9 @@
-numList = [0 for i in range(31)]
+numList = [0 for _ in range(31)]
 
 for count in range(28):
-    numList.append(int(input()))
+    count = int(input())
+    numList.append(count)
 
 for i in range(1,31):
-    if i == 0:
+    if numList[i] == 0:
         print(numList.index[i])
