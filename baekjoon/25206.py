2eb7282c8158207879347bd5f20f11ca91d328a2
songyaeji <skaehdlf0318@gmail.com>
2025-09-23 14:24:44 +0900

    25206 test 1

---
+++ baekjoon/25206.py
@@ -0,0 +1,41 @@
+import sys
+
+# [[course, credit, gpa],[course, credit, gpa]]
+gpaList = []
+lines = sys.stdin.readlines()
+for line in lines:
+    gpaList = map(line.split())
+
+for oneList in gpaList:
+    for course, credit, gpa in oneList:
+        if gpa == "P":
+            del oneList
+        elif gpa == "A+":
+            gpa = 4.5
+        elif gpa == "A0":
+            gpa = 4.0
+        elif gpa == "B+":
+            gpa = 3.5
+        elif gpa == "B0":
+            gpa = 3.0
+        elif gpa == "C+":
+            gpa = 2.5
+        elif gpa == "C0":
+            gpa = 2.0
+        elif gpa == "D+":
+            gpa = 1.5
+        elif gpa == "D0":
+            gpa = 1.0
+        else:
+            gpa = 0.0
+
+
+sumCredit = 0.0
+sumGPA = 0.0
+for oneList in gpaList:
+    for course, credit, gpa in oneList:
+        sumCredit += credit
+        sumGPA = credit*gpa
+
+totalGPA = sumGPA/sumCredit
+print(totalGPA)
