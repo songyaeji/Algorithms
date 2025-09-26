39ddd210db24a81ec7904dfa744cb245ce1177d7
songyaeji <skaehdlf0318@gmail.com>
2025-09-26 23:56:51 +0900

    1920 test 2 - complete!

--- baekjoon/1920.py
+++ baekjoon/1920.py
@@ -1,12 +1,12 @@
 n = int(input())
-nList = list(map(int, input().split()))
+nSet = set(list(map(int, input().split())))
 m = int(input())
 mList = list(map(int, input().split()))
 
 answer = []
 
 for i in mList:
-    if i in nList:
+    if i in nSet:
         answer.append(1)
     else:
         answer.append(0)
