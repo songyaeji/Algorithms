7965df0deaeadca240454d59a914e887138a6975
songyaeji <skaehdlf0318@gmail.com>
2025-09-27 14:35:02 +0900

    18110 test 3

--- baekjoon/18110.py
+++ baekjoon/18110.py
@@ -1,11 +1,17 @@
 count = int(input())
 
+def r_(n):
+    if n - int(n) >= 0.5:
+        return int(n) + 1
+    else:
+        return int(n)
+
 if count == 0:
     print(0)
 else:
     inputList = [int(input()) for _ in range(count)]
     inputList.sort()
-    cut = round(count*0.15)
+    cut = r_(count*0.15)
     levelList = inputList[cut:(count-cut)]
-    average = round(sum(levelList)/len(levelList))
+    average = r_(sum(levelList)/len(levelList))
     print(average)
