2f07052bb49315ed08e104186fd0cb77eef690d0
songyaeji <skaehdlf0318@gmail.com>
2025-09-27 14:40:24 +0900

    18110 test 4 - complete!

--- baekjoon/18110.py
+++ baekjoon/18110.py
@@ -1,15 +1,18 @@
-count = int(input())
+import sys
+read = sys.stdin.readline
 
 def r_(n):
     if n - int(n) >= 0.5:
         return int(n) + 1
     else:
         return int(n)
+    
+count = int(read())
 
 if count == 0:
     print(0)
 else:
-    inputList = [int(input()) for _ in range(count)]
+    inputList = [int(read()) for _ in range(count)]
     inputList.sort()
     cut = r_(count*0.15)
     levelList = inputList[cut:(count-cut)]
