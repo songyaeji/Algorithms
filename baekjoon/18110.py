511b0d1d61a1f77ddd5914b0e001cb75c23a45e2
songyaeji <skaehdlf0318@gmail.com>
2025-09-27 14:15:48 +0900

    18110 test 2

--- baekjoon/18110.py
+++ baekjoon/18110.py
@@ -1,11 +1,11 @@
 count = int(input())
-inputList = [int(input()) for _ in range(count)]
-inputList.sort()
 
-cut = round(count*0.15)
-
-levelList = inputList[cut:(count-cut)]
-
-average = round(sum(levelList)/len(levelList))
-
-print(average)
+if count == 0:
+    print(0)
+else:
+    inputList = [int(input()) for _ in range(count)]
+    inputList.sort()
+    cut = round(count*0.15)
+    levelList = inputList[cut:(count-cut)]
+    average = round(sum(levelList)/len(levelList))
+    print(average)
