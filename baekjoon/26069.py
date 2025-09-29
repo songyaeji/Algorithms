b20a9bbd56df2f644da5d045f016a2d29ca166b2
songyaeji <skaehdlf0318@gmail.com>
2025-09-29 19:32:28 +0900

    26069 test 2 - complete!

--- baekjoon/26069.py
+++ baekjoon/26069.py
@@ -7,8 +7,9 @@ rainbow.add("ChongChong")
 n = int(read())
 for i in range(n):
     people = read().strip().split()
-    if people[0] == "ChongChong":
+    if people[0] in rainbow and people[1] not in rainbow:
         rainbow.add(people[1])
-    rainbow.add(people[1])
+    elif people[1] in rainbow and people[0] not in rainbow:
+        rainbow.add(people[0])
 
 print(len(rainbow))
