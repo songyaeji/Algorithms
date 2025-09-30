678a8a1841d2a61f3b96c4aac171d5d1a58a0a4b
songyaeji <skaehdlf0318@gmail.com>
2025-09-30 23:27:13 +0900

    20920 test 1

---
+++ baekjoon/20920.py
@@ -0,0 +1,33 @@
+import sys
+read = sys.stdin.readline
+
+n, standard = read().strip().split()
+n = int(n)
+standard = int(standard)
+
+vocabList = []
+vocabDict = {}
+
+for _ in range(n):
+    word = read().strip()
+    if len(word) >= standard:
+        if word not in vocabDict:
+            vocabDict[word] = 1
+        else:
+            vocabDict[word] += 1
+    else:
+        continue
+
+vocabDict = sorted(vocabDict.items(), key = lambda x: x[1], reverse=True)
+vocabList = vocabDict.keys()
+
+for i in range(len(vocabList)):
+    if vocabDict[vocabList[i]] == vocabDict[vocabList[i+1]]:
+        if len(vocabList[i]) < len(vocabList[i+1]):
+            vocabList[i], vocabList[i+1] = vocabList[i+1], vocabList[i]
+        else:
+            continue
+    else:
+        continue
+
+print(*vocabList, sep='\n')
