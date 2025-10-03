13047fec710b1bdbd517cc007ab17599fd2ea84b
songyaeji <skaehdlf0318@gmail.com>
2025-10-03 14:41:02 +0900

    5052 test 5 - complete!

--- baekjoon/5052.py
+++ baekjoon/5052.py
@@ -2,24 +2,19 @@ import sys
 read = sys.stdin.readline
 
 caseCount = int(read())
-
 answer = []
 
 for _ in range(caseCount):
     numberCount = int(read())
-    numberList = [0 for _ in range(numberCount)]
-    for i in range(numberCount):
-        number = read().strip()
-        numberList[i] = number
+    numberList = [read().strip() for _ in range(numberCount)]
     numberList.sort()
-    for j in range(numberCount-1):
-        ending = len(numberList[j])
-        for k in range(j+1, numberCount):
-            if numberList[j] == numberList[k][:ending]:
-                answer.append("NO")
-                exit(0)
-            else:
-                answer.append("YES")
-                exit(0)
+    isConsistent = True
+    for j in range(numberCount - 1):
+        if numberList[j] == numberList[j+1][:len(numberList[j])]:
+            answer.append("NO")
+            isConsistent = False
+            break
+    if isConsistent:
+        answer.append("YES")
 
 print(*answer, sep='\n')
