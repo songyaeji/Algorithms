7274b0b851e41debf98414937bcc6c0c022b0faf
songyaeji <skaehdlf0318@gmail.com>
2025-10-03 13:56:02 +0900

    5052 test 1

---
+++ baekjoon/5052.py
@@ -0,0 +1,18 @@
+import sys
+read = sys.stdin.readline
+
+caseCount = int(read())
+
+for _ in range(caseCount):
+    numberCount = int(read())
+    numberList = [0 for _ in range(numberCount)]
+    for i in range(numberCount):
+        number = read().strip()
+        numberList[i] = number
+    numberList.sort()
+    for j in range(len(numberList)-1):
+        ending = len(numberList[j])
+        if numberList[j] == numberList[j+1][:ending]:
+            print("NO")
+        else:
+            print("YES")
